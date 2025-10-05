ed3b50cfa721abf1d396fb8a7645e5d96b69d413
Ekaterina-Smirnova22 <katushkasmirnova22@gmail.com>
2025-10-05 11:59:43 +0500

    lab_4 4.py

--- lab_4 4.py
+++ lab_4 4.py
@@ -1,1 +1,11 @@
+def main(x, *args):
+    one = x
+    two = sum(args)
+    three = float(len(args))
+    print (f"one={one}\ntwo={two}\nthree={three}")
 
+    return x + sum(args) / float(len(args))
+
+if __name__ == '__main__':
+    result = main(5, 1, 1, 0, -1, 0, 1, 2)
+    print(f"\nresult={result}")
