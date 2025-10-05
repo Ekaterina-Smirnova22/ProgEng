d8198a1ee54af50184ca69802a9c33c27e2b614d
Ekaterina-Smirnova22 <katushkasmirnova22@gmail.com>
2025-10-05 11:56:12 +0500

    lab_4 3.py

--- lab_4 3.py
+++ lab_4 3.py
@@ -1,1 +1,10 @@
+def main(one, two):
+     result = one + two
+     return result
 
+if __name__ == '__main__':
+    for i in range(5):
+        x =1
+        y =10
+        answer = main(x, y)
+        print(answer)
