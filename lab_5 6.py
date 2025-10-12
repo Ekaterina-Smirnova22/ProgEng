555ef472a818f6d308e02d47ce05bcea2bf110ac
Ekaterina-Smirnova22 <katushkasmirnova22@gmail.com>
2025-10-12 22:30:05 +0500

    Add files via upload

---
+++ lab_5 6.py
@@ -0,0 +1,7 @@
+superheroes = ['superman', 'spiderman', 'batman']
+
+nikolay, vasiliy, ivan = superheroes
+
+print('Николай - ', nikolay)
+print('Василий -', vasiliy)
+print('Иваан -', ivan)
---
+++ sm_5 1.py
@@ -0,0 +1,18 @@
+restaurant_bill = [8734, 2345, 8201, 6621, 9999, 1234, 5678, 8201, 8888, 4321, 3365,
+                   1478, 9865, 5555, 7777, 9998, 1111, 2222, 3333, 4444, 5556, 6666,
+                   5410, 7778, 8889, 4445, 1439, 9604, 8201, 3365, 7502, 3016, 4928,
+                   5837, 8201, 2643, 5017, 9682, 8530, 3250, 7193, 9051, 4506, 1987,
+                   3365, 5410, 7168, 7777, 9865, 5678, 8201, 4445, 3016, 4506, 4506]
+
+from collections import Counter
+
+unique_visitors = set(restaurant_bill)
+col_unique_visitors = len(unique_visitors)
+print("Всего ресторан посетили", col_unique_visitors, "человек")
+
+checks = len(restaurant_bill)
+print("Было выдано", checks, "чеков")
+
+worker_counts = Counter(restaurant_bill)
+active_worker = worker_counts.most_common(1)[0][0]
+print("Работник, посещавший ресторан чаще всего:", active_worker)
