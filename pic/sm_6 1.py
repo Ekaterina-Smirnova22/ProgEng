2e9eea700a2c11cbaf50c8a2bd8e332d86b1a213
Ekaterina-Smirnova22 <katushkasmirnova22@gmail.com>
2025-10-16 18:45:04 +0500

    Delete pic directory

--- pic/sm_6 1.py
+++
@@ -1,7 +0,0 @@
-marial = input ('Ведите числа через пробел: ')
-
-numbers_list = list(map(int, marial.split()))
-numbers_tuple = tuple(numbers_list)
-
-print('Список: ', numbers_list)
-print('Кортеж: ', numbers_tuple)
